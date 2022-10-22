bug fixed! (normalization should be divided after all loop ends instead of divided by each procs)

ELPH/EX_PH_scat.py
@@ -61,6 +61,7 @@ def Gamma_scat(Q_kmap=6, n_ext_acv_index=0,T=100, degaussian=0.001,muteProgress=
     # it seems that we can directly add first and second together, so we don't need Gamma_res
     Gamma_res = 0
     # Since Gamma_first and Gamma_second don't share same renormalization factor, so we need to split them
+
     Gamma_first_res = 0
     Gamma_second_res = 0
     factor = 2*np.pi/(h_bar*Nqpt) # dimension = [eV-1.s-1]
@@ -71,9 +72,11 @@ def Gamma_scat(Q_kmap=6, n_ext_acv_index=0,T=100, degaussian=0.001,muteProgress=
     #=============================
     # todo: double check if this is right (parallel unit):
     if q_map_start_para == 'nopara' and q_map_end_para == 'nopara':
+        status_for_para = 'nopara'
         q_map_start_para = 0
         q_map_end_para = kmap.shape[0]
     else:
+        status_for_para = 'para'
         if type(q_map_start_para) is int and type(q_map_end_para) is int:
             pass
         else:
@@ -149,11 +152,21 @@ def Gamma_scat(Q_kmap=6, n_ext_acv_index=0,T=100, degaussian=0.001,muteProgress=
                 Gamma_second_res = Gamma_second_res + factor * gqQ_sq_temp * distribution_second_temp
 
     # return Gamma_res
-
+    # IF this is a series test:
+    if status_for_para == 'nopara':
+        return Gamma_first_res / dirac_normalize_factor_first + Gamma_second_res / dirac_normalize_factor_second
+        # Warniing: only for debug
+        # return Gamma_first_res + Gamma_second_res
     # (a) w/ normalization
-    return Gamma_first_res/dirac_normalize_factor_first + Gamma_second_res/dirac_normalize_factor_second
-    # (b) w/o normalization
-    # TODO: TODO: TODO: do not use this!!
+    # return Gamma_first_res/dirac_normalize_factor_first + Gamma_second_res/dirac_normalize_factor_second
+    # Warning: normalization factor should be added at the last step
+    else:
+        return  [Gamma_first_res, Gamma_second_res,dirac_normalize_factor_first,dirac_normalize_factor_second]
+        #Warniing: only for debug
+        #return [Gamma_first_res, Gamma_second_res,1,1]
+
+    # (b) w/o normalization deprecated debug:
+    # TODOdone: TODOdone: TODOdone: do not use this!!
     # return Gamma_first_res + Gamma_second_res
 if __name__ == "__main__":
     res = Gamma_scat(Q_kmap=15, n_ext_acv_index=2,T=100, degaussian=0.001,path='../')

Parallel/Para_common.py
@@ -50,31 +50,35 @@ def before_parallel_job(rk,size, workload_para):
     else:
         return None, None, None
 
-def after_parallel_sum_job(rk,size,receive_res,start_time,start_time_proc):
+def after_parallel_sum_job(rk,size,receive_res,start_time,start_time_proc, mute=False):
     if rk == 0:
         value = receive_res[0]
+        if not mute:
+            print('type of value',type(value))
 
-        print('type of value',type(value))
+            print('===================================')
+            print('process= %d is summarizing ' % rk)
 
-        print('===================================')
-        print('process= %d is summarizing ' % rk)
         for k in range(1,size):
             value = value + receive_res[k]
         if type(value) is not np.ndarray:
-            print("sub_res is", receive_res)
-            print("res is",value)
+            if not mute:
+                print("sub_res is", receive_res)
+                print("res is",value)
 
         else:
             # which means value is a matrix, and head of res is usually stored at [0,-1]
             # print("head of sub_res is", receive_res[k][0, -1])
-            print("head of res is", value[0, -1])
-            print("tail of res is", value[-1, -1])
+            if not mute:
+                print("head of res is", value[0, -1])
+                print("tail of res is", value[-1, -1])
         end_time = time.time()
         end_time_proc = process_time()
-        print("the wall time is: %.3f s"%(end_time - start_time))
-        print("the proc time is: %.3f s"%(end_time_proc - start_time_proc))
-        print('===================================')
-        print('hello')
+        if not mute:
+            print("the wall time is: %.3f s"%(end_time - start_time))
+            print("the proc time is: %.3f s"%(end_time_proc - start_time_proc))
+            print('===================================')
+            print('hello')
         return value
     else:
         pass