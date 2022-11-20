a904e4225c9051170dca285cb6279235a02cc62e
bwhou <bowen.hou@yale.edu>
2022-11-20 16:02:53 -0500

    finite_any_Q_BSE.py, finite_any_Q_DFT.py and finite-Q-instruction.md are added

---
+++ Utilities/finite_any_Q_BSE.py
@@ -0,0 +1,150 @@
+import os
+import numpy as np
+# Setting
+
+nv = 2
+nc = 2
+
+qos = 'development'
+nodes = 40
+cores = 54
+time = "02:00:00"
+
+inteqp_root = "number_val_bands_fine %s\nnumber_val_bands_coarse %s\n\nnumber_cond_bands_fine %s\nnumber_cond_bands_coarse %s\n\nno_symmetries_fine_grid\nno_symmetries_shifted_grid\nno_symmetries_coarse_grid\n\nuse_momentum\n" % (
+nv, nv, nc, nc)
+inteqp_each = "number_val_bands_fine %s\nnumber_val_bands_coarse %s\n\nnumber_cond_bands_fine %s\nnumber_cond_bands_coarse %s\n\nno_symmetries_fine_grid\nno_symmetries_shifted_grid\nuse_symmetries_coarse_grid\n\nuse_momentum\n" % (
+nv, nv, nc, nc)
+
+kernel = "number_val_bands %s\nnumber_cond_bands %s\n\nuse_symmetries_coarse_grid\nenergy_loss\nscreening_semiconductor\ncell_slab_truncation\n\nexciton_Q_shift 0 " % (
+nv, nc)
+absorption = "number_val_bands_fine %s\nnumber_val_bands_coarse %s\n\nnumber_cond_bands_fine %s\nnumber_cond_bands_coarse %s\n\nuse_symmetries_fine_grid\nno_symmetries_shifted_grid\nuse_symmetries_coarse_grid\n\neqp_co_q_corrections\neqp_co_corrections\n\ndiagonalization\nscreening_semiconductor\ncell_slab_truncation\n\n\nuse_velocity\nenergy_resolution 0.05\ngaussian_broadening\n\nwrite_eigenvectors 10\n\nexciton_Q_shift 0 " % (
+nv, nv, nc, nc)
+
+# a = ('cd 1-epsilon\n', 'srun -n %d --cpu_bind=cores $BGWPATH1/epsilon.cplx.x > epsilon.out\n'%(nodes*cores)")
+
+
+########################################################################################################################################################################################################################################################
+# Reading Parameter
+# os.system("wfn_rho_vxc_info.x ../1-mf/4.1-wfn_co_fullgrid/WFN > ./temp")
+# f = open("temp", 'r')
+#
+# while True:
+#     line = f.readline()
+#     #    print(line)
+#     if "Number of k-points:" in line:
+#         n_kpt = int(line.split()[-1])
+#     if "   Index         Coordinates" in line:
+#         break
+#
+# k_list = []
+# for i in range(n_kpt):
+#     line = f.readline()
+#     temp = line.split()[1] + ' ' + line.split()[2] + ' ' + line.split()[3]
+#     k_list.append(temp)
+#
+# f.close()
+#
+# shift = k_list[0]
+#
+# shift = [float(x) for x in shift.split()]
+# k_list_new = []
+# for i in range(len(k_list)):
+#     temp_1 = k_list[i].split()
+#     temp_2 = [float(x) for x in temp_1]
+#     for j in range(len(temp_2)):
+#         temp_2[j] = temp_2[j] - shift[j]
+#
+#     k_list_new.append("%.6f %.6f %.6f" % (-1 * temp_2[0], -1 * temp_2[1], -1 * temp_2[2]))
+# k_list = k_list_new
+
+k_list_raw = np.loadtxt('../1-mf/kpt.dat')
+k_list_new = []
+for i in range(k_list_raw.shape[0]):
+    k_list_new.append("%.6f %.6f %.6f" % (-1 * k_list_raw [i,0], -1 * k_list_raw [i,1], -1 * k_list_raw [i,2]))
+k_list = k_list_new
+n_kpt = len(k_list)
+
+# os.system("rm temp")
+os.system("mkdir 5-exciton-Q")
+# os.system("mv temp ./5-exciton-Q/kpt.dat")
+os.system("mkdir 5-exciton-Q/inteqp")
+f = open("inteqp.inp", 'w')
+f.write(inteqp_root)
+f.close()
+
+os.system("mv inteqp.inp 5-exciton-Q/inteqp/")
+os.chdir("./5-exciton-Q/inteqp")
+os.system("ln -sf ../../../1-mf/2.1-wfn/WFN ./WFN_co")
+os.system("ln -sf ../../../1-mf/4.1-wfn_co_fullgrid/WFN ./WFN_fi")
+os.system("ln -sf ../../2-sigma/eqp1.dat ./eqp_co.dat")
+
+os.chdir("../")
+for i in range(len(k_list)):
+    print(i+1, '/', len(k_list))
+
+    os.system("mkdir Q-%s" % (i + 1))
+    os.system("mkdir Q-%s/5.0-inteqp-Q" % (i + 1))
+    os.system("mkdir Q-%s/5.1-kernel-Q" % (i + 1))
+    os.system("mkdir Q-%s/5.2-absorp-Q" % (i + 1))
+    os.chdir("./Q-%s" % (i + 1))
+    os.system("echo %s > kpt" % k_list[i])
+
+    os.chdir("./5.0-inteqp-Q")
+    os.system("ln -sf ../../../../1-mf/2.1-wfn/WFN ./WFN_co")
+    os.system("ln -sf ../../../../1-mf/4.2-wfnq_co-%s/WFN ./WFN_fi"% (i + 1))
+    os.system("ln -sf ../../../../1-mf/4.2-wfnq_co-%s/WFN ./WFNq_fi"% (i + 1))
+    os.system("ln -sf ../../../2-sigma/eqp1.dat ./eqp_co.dat")
+    f = open("inteqp.inp", 'w')
+    f.write(inteqp_each)
+    f.close()
+    os.chdir('../')
+
+    os.chdir("./5.1-kernel-Q")
+    os.system("ln -sf ../../../1-epsilon/eps0mat.h5 ./")
+    os.system("ln -sf ../../../1-epsilon/epsmat.h5 ./")
+    os.system("ln -sf ../../../../1-mf/4.1-wfn_co_fullgrid/WFN ./WFN_co")
+    os.system("ln -sf ../../../../1-mf/4.2-wfnq_co-%s/WFN ./WFNq_co" % (i + 1))
+    kernel_temp = kernel + k_list[i]
+    f = open("kernel.inp", 'w')
+    f.write(kernel_temp)
+    f.close()
+    os.chdir("../5.2-absorp-Q")
+    os.system("ln -sf ../5.1-kernel-Q/bsemat.h5 ./")
+    os.system("ln -sf ../../../1-epsilon/eps0mat.h5 ./")
+    os.system("ln -sf ../../../1-epsilon/epsmat.h5 ./")
+    os.system("ln -sf ../../../../1-mf/4.1-wfn_co_fullgrid/WFN ./WFN_co")
+    os.system("ln -sf ../../../../1-mf/4.1-wfn_co_fullgrid/WFN ./WFN_fi")
+    os.system("ln -sf ../../../../1-mf/4.2-wfnq_co-%s/WFN ./WFNq_co" % (i + 1))
+    os.system("ln -sf ../../../../1-mf/4.2-wfnq_co-%s/WFN ./WFNq_fi" % (i + 1))
+    os.system("ln -sf ../../inteqp/eqp.dat ./eqp_co.dat")
+    os.system("ln -sf ../5.0-inteqp-Q/eqp.dat ./eqp_co_q.dat")
+    absorption_temp = absorption + k_list[i]
+    f = open("absorption.inp", 'w')
+    f.write(absorption_temp)
+    f.close()
+
+    os.chdir("../../")
+
+go_sh_tacc = ['#!/bin/bash\n', '#SBATCH -J fin_Q\n', '#SBATCH -o myjob.o\%j\n' '#SBATCH -e myjob.e\%j\n',
+
+              '#SBATCH -p %s\n' % qos, '#SBATCH -N %d\n' % nodes, '#SBATCH -n %d\n' % (nodes * cores),
+              '#SBATCH -t %s\n' % time, '#SBATCH -A PHY20032\n', '\n',
+
+              "#QEPATH='/global/homes/b/bwhou/software/qe-6.7/bin'\n",
+
+              "BGWPATH1='/home1/08237/bwhou/software/BerkeleyGW-3.0.1/bin/'\n", '\n\n',
+              "cd inteqp\nibrun $BGWPATH1/inteqp.cplx.x > inteqp.out\n",
+              "cd ../\n",
+              "for ((i=2;i<=%s;i++));\ndo\n" % n_kpt,
+              "cd ./Q-$i/5.1-kernel-Q\nibrun   $BGWPATH1/kernel.cplx.x > kernel.out\n",
+              "cd ../5.2-absorp-Q\n", "ibrun  $BGWPATH1/absorption.cplx.x > absorption.out\n", "cd ../../\ndone"]
+
+go = open('go.sh', 'w')
+for go_line in go_sh_tacc:
+    go.write(go_line)
+go.close()
+
+os.system("chmod +x go.sh")
+
+# mkdir for each kpoint
+
--- Utilities/finite_any_Q_DFT.py
+++ Utilities/finite_any_Q_DFT.py
@@ -42,6 +42,13 @@ def pp_inq_list(pp_inq_lines,Q_shift):
             pp_inq_lines[j] = '   wfng_dk3 = %.4f \n' % (wfng_nk1 * Q_shift[2])
     return pp_inq_lines
 
+def pp_inq_list_2(pp_inq_lines):
+    for j,line in enumerate(pp_inq_lines):
+        # if 'wfng_nk' in line or 'wfng_dk' in line:
+        if 'wfng_dk' in line:
+            pp_inq_lines[j] = ''
+    return pp_inq_lines
+
 def write_list_line_by_line(a_list, file_name):
     f = open(file_name,'w')
     for i in range(len(a_list)):
@@ -49,8 +56,45 @@ def write_list_line_by_line(a_list, file_name):
     f.close()
 
 
+def get_unfold_grid_from_klog():
+    """
+    :return: list to be passed to wrte_list_line_by_line.
+    Read unfolded kgrid from kgrid.log
+    """
+    f = open('kgrid.log','r')
+    lines = f.readlines()
+    nk_index = "None"
+    start_index = "None"
+    for i, line in enumerate(lines):
+        if "k-points in the unfolded Brillouin Zone" in line:
+            start_index = i
+            nk_index = i+1
+            break
+    output_list = ["K_POINTS crystal\n", lines[nk_index]]
+    for i in range(int(lines[nk_index])):
+        j = i + start_index + 2
+        output_list.append(' '.join(lines[j].split()[1:])+'\n')
+    return output_list
 
-
+def get_unfold_grid_from_klog_without_shift():
+    """
+    :return: list to be passed to wrte_list_line_by_line.
+    Read unfolded kgrid from kgrid.log
+    """
+    f = open('kgrid.log','r')
+    lines = f.readlines()
+    nk_index = "None"
+    start_index = "None"
+    for i, line in enumerate(lines):
+        if "k-points in the original uniform grid" in line:
+            start_index = i
+            nk_index = i+1
+            break
+    output_list = ["K_POINTS crystal\n", lines[nk_index]]
+    for i in range(int(lines[nk_index])):
+        j = i + start_index + 2
+        output_list.append(' '.join(lines[j].split()[1:4])+' 1.0\n')
+    return output_list
 
 if __name__ == "__main__":
     # write_list_line_by_line(kgrid_inp_list(kgrid_lines,Q_shift_list[2]), 'test.inp')
@@ -58,14 +102,14 @@ if __name__ == "__main__":
 
     # (0) Checking and Loading
     # (i) check in_head/pp_in (2.1-wfn), inq_head/pp_inq(2.2-wfnq), kgrid.inp, kpt.dat(get from 4-path/WFN)
-    if os.path.isfile('../test/in_head') and os.path.isfile('../test/inq_head') and os.path.isfile('pp_in') and os.path.isfile('pp_inq') and os.path.isfile(
-            '../test/kgrid.inp') and os.path.isfile('../test/kpt.dat'):
+    if os.path.isfile('./in_head') and os.path.isfile('./inq_head') and os.path.isfile('pp_in') and os.path.isfile('pp_inq') and os.path.isfile(
+            './kgrid.inp') and os.path.isfile('./kpt.dat'):
         print(('in_head/pp_in, inq_head/pp_inq, pp_in and pp_inq found'))
     else:
         raise Exception("in_head/pp_in, inq_head/pp_inq, pp_in and pp_inq is missing")
     Q_shift_list = np.loadtxt('kpt.dat')
     n_Q_shift = Q_shift_list.shape[0]
-    kgrid_inp = open('../test/kgrid.inp', 'r')
+    kgrid_inp = open('./kgrid.inp', 'r')
     kgrid_lines = kgrid_inp.readlines()
 
 
@@ -80,11 +124,12 @@ if __name__ == "__main__":
     print('building 4.1-wfn_co_fullgrid')
     os.system('mkdir ./4.1-wfn_co_fullgrid')
     # print(os.getcwd())
-    os.chdir('../test/4.1-wfn_co_fullgrid')
+    os.chdir('./4.1-wfn_co_fullgrid')
     # print(os.getcwd())
-    write_list_line_by_line(kgrid_inp_list(kgrid_lines=kgrid_lines, Q_shift=-1*np.array([0,0,0])), file_name='../test/kgrid.inp')
+    write_list_line_by_line(kgrid_inp_list(kgrid_lines=kgrid_lines, Q_shift=-1*np.array([0,0,0])), file_name='./kgrid.inp')
     os.system('cp ../in_head ./in')
     os.system('kgrid.x kgrid.inp kgrid.out kgrid.log')
+    # write_list_line_by_line(get_unfold_grid_from_klog() ,'kgrid_unfold.out')
     os.system('cat kgrid.out >> in')
     os.system('cp -r ../1-scf/pp ./')
     os.system('cp ../pp_in ./')
@@ -94,23 +139,31 @@ if __name__ == "__main__":
     os.system('ln -sf ../../1-scf/'+save_file+'/charge-density.dat ./')
     os.chdir('../../')
 
-
+    # print(Q_shift_list)
     # (2) write a loop to build 4.2-wfnq_co-n (w/ Q shift)
     for i in range(n_Q_shift):
         print('building 4.2-wfnq_co-%s out of %s'% (i+1,n_Q_shift))
         os.system('mkdir 4.2-wfnq_co-%s'%(i+1))
         os.chdir('4.2-wfnq_co-%s'%(i+1))
-        write_list_line_by_line(kgrid_inp_list(kgrid_lines=kgrid_lines,Q_shift=-1*Q_shift_list[i]), file_name='../test/kgrid.inp')
+        write_list_line_by_line(kgrid_inp_list(kgrid_lines=kgrid_lines,Q_shift=-1*Q_shift_list[i]), file_name='./kgrid.inp')
         os.system('cp ../inq_head ./in')
         os.system('kgrid.x kgrid.inp kgrid.out kgrid.log')
-        os.system('cat kgrid.out >> in')
-        os.system('cp -r ../1-scf/pp ./')
-        os.system('cp ../pp_inq ./pp_inq') ##todo: repace some part of pp_inq
 
+        if np.all(Q_shift_list[i] == 0.):
+            # print("np.all(Q_shift_list[%s]) == 0"%i)
+            write_list_line_by_line(get_unfold_grid_from_klog_without_shift(), 'kgrid_unfold.out')
+            # os.system('cat kgrid.out >> in')
+        else:
+            write_list_line_by_line(get_unfold_grid_from_klog(), 'kgrid_unfold.out')
+        os.system('cat kgrid_unfold.out >> in')
+
+        os.system('cp -r ../1-scf/pp ./')
+        os.system('cp ../pp_inq ./pp_inq') ##tododone: repace some part of pp_inq
         # read pp
         pp_inq = open('pp_inq', 'r')
         pp_inq_lines = pp_inq.readlines()
-        write_list_line_by_line(pp_inq_list(pp_inq_lines=pp_inq_lines, Q_shift=-1*Q_shift_list[i]),'pp_in')
+        write_list_line_by_line(pp_inq_list_2(pp_inq_lines=pp_inq_lines),'pp_in') # this is for shift of dkg 1,2,3
+        # write_list_line_by_line(pp_inq_lines, 'pp_in')
 
         os.system('mkdir ' + save_file)
         os.chdir(save_file)
