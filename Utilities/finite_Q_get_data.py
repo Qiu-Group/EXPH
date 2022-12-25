d2e1feb63edaa8430f9eb9b2be83355403bd89bd
bwhou <bowen.hou@yale.edu>
2022-12-25 10:08:12 -0500

    finite_Q_get_data.py is reformated

--- Utilities/finite_Q_get_data.py
+++ Utilities/finite_Q_get_data.py
@@ -3,17 +3,26 @@
 import os
 
 
-nQ = 1
-number_kpt = 91
+nS = 1
+number_kpt = 576
 
 os.chdir('5-exciton-Q')
-os.system("echo 'eigenvalues' > xc_%s.dat "%nQ)
+os.system("echo 'eigenvalues' > xc_%s.dat "%nS)
 os.system("echo 'kx ky kz' > kpt_crystal.dat")
 for i in range(1,number_kpt + 1):
     print(i)
-    if os.path.isfile("Q-%s/5.2-absorp-Q/eigenvalues.dat"%i):
-        os.system("sed -n %sp Q-%s/5.2-absorp-Q/eigenvalues.dat|awk '{print $1}' >> xc_%s.dat"%(nQ+4, i, nQ))
+    if i == 1:
+        if os.path.isfile("Q-%s/5.2-absorp-Q/eigenvalues_b1.dat" % i):
+            os.system("sed -n %sp Q-%s/5.2-absorp-Q/eigenvalues_b1.dat|awk '{print $1}' >> xc_%s.dat" % (nS + 4, i, nS))
+        else:
+            print("warning: eigenvalues not found")
+            os.system("echo none >> xc_%s.dat" % (nS))
+        os.system("cat Q-%s/kpt >> kpt_crystal.dat" % i)
+
     else:
-        print("warning: eigenvalues not found")
-        os.system("echo none >> xc_%s.dat" % (nQ))
-    os.system("cat Q-%s/kpt >> kpt_crystal.dat"%i)
+        if os.path.isfile("Q-%s/5.2-absorp-Q/eigenvalues.dat"%i):
+            os.system("sed -n %sp Q-%s/5.2-absorp-Q/eigenvalues.dat|awk '{print $1}' >> xc_%s.dat"%(nS+4, i, nS))
+        else:
+            print("warning: eigenvalues not found")
+            os.system("echo none >> xc_%s.dat" % (nS))
+        os.system("cat Q-%s/kpt >> kpt_crystal.dat"%i)
