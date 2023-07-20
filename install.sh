18eb4ccb5e3369c7bb6ea4ce5cb8df2884d07247
bwhou <bowen.hou@yale.edu>
2023-07-20 11:35:15 -0400

    add dat2h5_gkk to bin

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ ln -sf ../Utilities/finite_any_Q_DFT.py ./
 ln -sf ../Utilities/finite_Q_get_data.py ./
 ln -sf ../Utilities/finite_uniform_Q_BSE.py ./
 ln -sf ../Utilities/collect.py ./
+ln -sf ../Utilities/dat2h5_gkk.py
 
 chmod +x collect.py
 chmod +x exph.py
